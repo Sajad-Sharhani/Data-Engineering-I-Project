a994b545184da06f8c887834533d2d7327ea583e
fbtrost <84677958+fbtrost@users.noreply.github.com>
2023-03-18 21:22:27 +0100

    Minor tweaks

--- Kubernetes/setup_agent.sh
+++ Kubernetes/setup_agent.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 curl https://releases.rancher.com/install-docker/20.10.sh | sh
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.2.129:6443 K3S_TOKEN=K10912651b0efe2e80e060d0346d0f2ec6a20539b08aff883422aa62daf497829bb::server:a4176ae0b1dd7085becd76ac9c2925a1 sh -s - --docker
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.2.129:6443 K3S_TOKEN=K107ab62d2cfcb691dcc6a84debf8f904a0b66acfddb03c997bd1699c2d0ecc5786::server:8c5fee8a7bec2b9fa03b441fa3aa4ff9 sh -s - --docker
 
 
 
